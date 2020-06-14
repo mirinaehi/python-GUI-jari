330b463e8bf77f8f1a69c2d29b78907287a884d9
mirinaehi <garook@e-mirim.hs.kr>
2020-06-14 12:08:46 +0900

    버튼과 툴팁

--- 파이썬GUI1.py
+++ 파이썬GUI1.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
+from PyQt5.QtGui import QFont
+
 
 class MyQpp(QWidget):
     def __init__(self):
@@ -7,7 +9,15 @@ class MyQpp(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('첫번째 앱')
+        QToolTip.setFont(QFont('SansSerif', 10))
+        self.setToolTip('This is a <b>전체화면</b> widget')
+
+        btn = QPushButton('버튼', self)
+        btn.setToolTip('This is a <b>버튼</b> widget')
+        btn.move(50, 50)
+        btn.resize(btn.sizeHint())
+
+        self.setWindowTitle('버튼만들기')
         self.setGeometry(300, 300, 400, 200)
         self.show()
 
