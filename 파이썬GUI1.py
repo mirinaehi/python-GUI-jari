bd83d1650e0fe0391210e61d218211ae73917ebc
mirinaehi <garook@e-mirim.hs.kr>
2020-06-14 18:21:50 +0900

    버튼을 누를때마다 해당버튼 사라지기

--- 파이썬GUI1.py
+++ 파이썬GUI1.py
@@ -5,34 +5,38 @@ from functools import partial
 
 class MyQpp(QWidget):
     # global bwidth, bheight, iwidth, iheight, rec
+    좌석 = [0 for _ in range(21)]
+
     def __init__(self):
         super().__init__()
         self.initUI()
 
-
     def initUI(self):
-        seats = [0 for _ in range(21)]
-        for i in range(1, 20+1):
-            seats[i] = QPushButton(f'번호 {i}', self)
-            seats[i].resize(bwidth, bheight)
-            seats[i].move(iwidth+(i-1)%6*bwidth + (i-1)%6//2*iwidth, 20+(i-1)//6*iheight)
-            #partial : 함수와 argument를 넘기기 위해 사용
-            seats[i].clicked.connect(partial(self.on_click, i))
+        for i in range(1, 20 + 1):
+            self.좌석[i] = QPushButton(f'번호 {i}', self)
+            self.좌석[i].resize(bwidth, bheight)
+            self.좌석[i].move(iwidth + (i - 1) % 6 * bwidth + (i - 1) % 6 // 2 * iwidth, 20 + (i - 1) // 6 * iheight)
+            # partial : 함수와 argument를 넘기기 위해 사용
+            self.좌석[i].clicked.connect(partial(self.on_click, i))
 
         self.setWindowTitle('absolute 포지션')
-        self.setGeometry(rec.width()//4, rec.height()//4, rec.width()//2, rec.height()//2)
+        self.setGeometry(rec.width() // 4, rec.height() // 4, rec.width() // 2, rec.height() // 2)
         self.show()
 
     def on_click(self, i):
-        print(i)
+        self.좌석[i].setEnabled(False)
+        self.좌석[i].move(9999, 9999)
+        # for m in range(i+6, 20+1, 6):
 
-#main 코드
+
+# main 코드
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    rec = app.desktop().geometry()      #모니터 크기 구하기
-    bwidth = rec.width()//20; bheight = rec.height()//20;      #버튼의 크기
-    iwidth = bwidth//2; iheight = bheight*1.8;     #버튼의 간격
+    rec = app.desktop().geometry()  # 모니터 크기 구하기
+    bwidth = rec.width() // 20;
+    bheight = rec.height() // 20;  # 버튼의 크기
+    iwidth = bwidth // 2;
+    iheight = bheight * 1.8;  # 버튼의 간격
 
     ex = MyQpp()
     sys.exit(app.exec_())
-
