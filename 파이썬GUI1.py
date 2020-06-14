08ca7e83111ca707c6f7173325a355f031e80422
mirinaehi <garook@e-mirim.hs.kr>
2020-06-14 11:47:29 +0900

    기본 창 띄우기

---
+++ 파이썬GUI1.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+class MyQpp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('첫번째 앱')
+        self.move(300, 300)
+        self.resize(400, 200)
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyQpp()
+    sys.exit(app.exec_())
+
