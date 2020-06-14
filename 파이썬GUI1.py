1fbeab3feb9e8ed10d6178dc67a7b45b332b5a62
mirinaehi <garook@e-mirim.hs.kr>
2020-06-14 22:22:55 +0900

    맨 뒷자리 오른쪽으로 배정 및 앞줄이 사라지지 않는 버그 해결

--- 파이썬GUI1.py
+++ 파이썬GUI1.py
@@ -11,18 +11,29 @@ def PickToken():
                 뽑기표[temp] +=i
                 break
 
+#순서대로 뽑기(디버그용)
+def PickTokenASC():
+    for i in range(1, 20+1):
+        뽑기표[i] = i
 
 class MyQpp(QWidget):
     # global bwidth, bheight, iwidth, iheight, rec
-    좌석 = [0 for _ in range(21)]
+    좌석 = [0 for _ in range(24+1)]
 
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        for i in range(1, 20 + 1):
+        for i in range(1, 18 + 1):
             self.좌석[i] = QPushButton(f'번호 {뽑기표[i]}', self)
+            self.좌석[i].resize(bwidth, bheight)
+            self.좌석[i].move(iwidth + (i - 1) % 6 * bwidth + (i - 1) % 6 // 2 * iwidth, 20 + (i - 1) // 6 * iheight)
+            # partial : 함수와 argument를 넘기기 위해 사용
+            self.좌석[i].clicked.connect(partial(self.on_click, i))
+
+        for i in range(23, 24+1):
+            self.좌석[i] = QPushButton(f'번호 {뽑기표[i-4]}', self)
             self.좌석[i].resize(bwidth, bheight)
             self.좌석[i].move(iwidth + (i - 1) % 6 * bwidth + (i - 1) % 6 // 2 * iwidth, 20 + (i - 1) // 6 * iheight)
             # partial : 함수와 argument를 넘기기 위해 사용
@@ -36,13 +47,18 @@ class MyQpp(QWidget):
         x축 = self.좌석[i].x()
 
         #맨 끝 위치 찾기
-        for m in range(i, 20+1, 6): pass
+        for m in range(i, 24+1, 6):
+            if 18<m<23:
+                m -=6
+                break
+
         for n in range(m, i, -6):
             for p in range(n-6, 0, -6):
-                if self.좌석[p].isEnabled():
+                if self.좌석[p].isEnabled() and self.좌석[n].isEnabled():
                     self.좌석[n].move(x축, self.좌석[p].y())
                     break
-                self.좌석[n].move(x축, 20)
+                if self.좌석[n].isEnabled():
+                    self.좌석[n].move(x축, 20)
 
         self.좌석[i].setEnabled(False)
         self.좌석[i].move(9999, 9999)
@@ -58,6 +74,6 @@ if __name__ == '__main__':
     iheight = bheight * 1.8;  # 버튼의 간격
     뽑기표 = collections.Counter()
 
-    PickToken()
+    PickTokenASC()
     ex = MyQpp()
     sys.exit(app.exec_())
